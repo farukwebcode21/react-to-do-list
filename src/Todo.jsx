3182ae7ac60dc08b2d56dae10242c3740f8013e5
farukwebcode21 <farukahmadmk@yahoo.com>
2023-11-06 21:41:08 +0600

    start to do list done

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
   const [activity, setActivity] = useState("");
   const [listData, setListData] = useState([]);
 
   const addActivity = () => {
-    setListData((listData) => {
-      const updatedList = [...listData, activity];
+    setListData((prevListData) => {
+      const updatedList = [...prevListData, activity];
       setActivity("");
       return updatedList;
     });
   };
 
   const removeActivity = (i) => {
-    const updatedListDta = listData.filter((elem, id) => {
-      return id != id;
+    const updatedListData = listData.filter((elem, id) => {
+      return id !== i;
     });
-    setListData(updatedListDta);
+    setListData(updatedListData);
   };
+
   return (
     <>
       <div className="container">
@@ -30,21 +30,19 @@ const Todo = () => {
           onChange={(e) => setActivity(e.target.value)}
         />
         <button onClick={addActivity}>Add</button>
-        <p className="list-heading">Here is your List:{")"}</p>
-        {listData != [] &&
-          listData.map((data, i) => {
-            return (
-              <div key={i}>
-                <p className="listData">{data}</p>
-                <button
-                  className="btn-position"
-                  onClick={() => removeActivity(i)}
-                >
-                  Remove
-                </button>
-              </div>
-            );
-          })}
+        <p className="list-heading">Here is your List:{"}"}</p>
+        {listData.length > 0 &&
+          listData.map((data, i) => (
+            <div key={data}>
+              <p className="listData">{data}</p>
+              <button
+                className="btn-position"
+                onClick={() => removeActivity(i)}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
       </div>
     </>
   );
